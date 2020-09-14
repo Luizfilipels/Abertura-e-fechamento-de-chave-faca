22e216a419d3c574edac247a207e40d57ea8859d
Luiz <luizfilipesl2003@gmail.com>
2020-09-13 22:20:40 -0300

    Update Source_Code.ino

--- Source_Code.ino
+++ Source_Code.ino
@@ -1,19 +1,29 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <Servo.h>
+#define sensorTensaoDC  A5
 
 Servo alfa;
 
 //Informações sobre IP, Gateway, mascara de rede e mac
 byte mac[] = { 0x70, 0xB3, 0xD5, 0x0A, 0xC6, 0x7B }; // <-- troque pelo seu MAC
-byte ip[] = { 10, 0, 0, 155 }; // <-- seu ip
-byte gateway[] = { 10, 0, 0, 1 };
+byte ip[] = { 192, 168, 1, 155 }; // <-- seu ip
+byte gateway[] = { 192, 168, 1, 1 };
 byte subnet[] = { 255, 255, 255, 0 };
 
 EthernetServer server(80);
 String readString;
 
-int status = 0;
+//variáveis
+int status = 1;
+float valorTensaoDC;
+int amostragem = 1000;
+float mediaTotalTensaoDC = 0;
+float valorFinalTensaoDC = 0;
+//valor resistor
+float voltsporUnidade = 0.004700000;
+float R1 = 30000.0;
+float R2 = 7500.0;
 
 void setup() {
   
@@ -29,7 +39,7 @@ void setup() {
 
   //atribuição do servo
   alfa.attach(12);
-  alfa.write(180);
+  alfa.write(80);
   
 }
 
@@ -61,15 +71,46 @@ void loop() {
 
           client.println("<HTML>");
           client.println("<HEAD>");
+          client.println("<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>");
+          client.println("<script src='http://portalink.net.br/gerencia/atualizar2.js'</script>");
           client.println("<TITLE>Controle de energia Arduino</TITLE>");
           client.println("</HEAD>");
           client.println("<BODY>");
-
-          client.println("<H1>Controle de energia</H1>");
+          client.println("<H1>Controle de energia e monitoramento de voltagem</H1>");
+                     
+                      //conta do voltimetro  
+          mediaTotalTensaoDC = 0;
+            for (int i = 0; i < amostragem ; i++) {
+              valorTensaoDC = analogRead(sensorTensaoDC);
+              valorTensaoDC = (valorTensaoDC * voltsporUnidade);
+              mediaTotalTensaoDC = mediaTotalTensaoDC + (valorTensaoDC / (R2 / (R1 + R2)));
+              
+            }
           
+          valorFinalTensaoDC = mediaTotalTensaoDC / amostragem;
+            Serial.print("Valor: ");
+            Serial.print(valorFinalTensaoDC);
+            Serial.print(" V");
+          
+          
+          client.println("________________________________");
+
+          
+          
+          client.println("<div id='setTime'>");
+          client.println("<br>");
+          //voltimentro
+          client.println("<br>Voltimentro: ");
+          client.println(valorFinalTensaoDC);
+          client.println(" V");
+          client.println("</div>");
+          client.println("<br>________________________________");
+          client.println("<br>");
+          client.println("<br>Controle de energia");
+          client.println("<br>");
           client.println("<a href=\"/?on\"\"><button>Ligar</button></a>"); 
           client.println("<a href=\"/?off\"\"><button>Desligar</button></a><br>"); 
-          client.println("<br>: ");
+          client.println("<br> ");
 
           
           
@@ -78,17 +119,17 @@ void loop() {
               // controle do arduino
           if(readString.indexOf("?on") >0)//checks for on
           {
-            alfa.write(80); // mude para o angulo de preferencia
-            Serial.println("Energia Ligada");
             client.println("Voce ligou a energia");
             status = 1;
+            alfa.write(80); // mude para o angulo de preferencia
+            
+
           }
           if(readString.indexOf("?off") >0)//checks for off
           {
-            alfa.write(180); // mude para o angulo de preferencia
-            Serial.println("Energia Desligada");
             client.println("Voce desligou a energia");
             status = 0;
+            alfa.write(180); // mude para o angulo de preferencia
           }
 
           client.println("<br>A energia esta atualmente: ");
@@ -103,11 +144,13 @@ void loop() {
           client.println("</BODY>");
           client.println("</HTML>");
           
-          delay(1);
+          delay(100);
           
           client.stop();
           readString="";
-
+          //COMANDOS QUE EU TIREI DO CLIENT:
+          //Serial.println("Energia Ligada");
+          //Serial.println("Energia Desligada");
         }
       }
     }
