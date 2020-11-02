ba6a2b4d554fc2b6fd941ff577ec06e4dd08a032
Luiz <luizfilipesl2003@gmail.com>
2020-11-02 13:43:35 -0300

    Update 02/11/2020
    Depois de colocar em produção, ele travou constantemente. Depois de analisar bem, sugeri que pode ser falta de SRAM, pois é apenas 2k de sram (Mancadona hein Arduino).
    
    Ai eu tive que reduzi o máximo que pude, sem claro, tirar as funções principais

---
+++ abertura_chave02112020.ino
@@ -0,0 +1,179 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <Servo.h>
+#define sensorTensaoDC  A5
+
+Servo alfa;
+
+//Informações sobre IP, Gateway, mascara de rede e mac
+byte mac[] = { 0x70, 0xB3, 0xD5, 0x0A, 0xC6, 0x7B }; // <-- troque pelo seu MAC
+byte ip[] = { 192, 168, 1, 165 }; // <-- seu ip
+byte gateway[] = { 192, 168, 1, 1 }; // <-- seu gateway
+byte subnet[] = { 255, 255, 255, 0 };
+
+EthernetServer server(80);
+String readString;
+
+//variáveis
+int status = 1;
+float valorTensaoDC;
+int amostragem = 1000;
+float mediaTotalTensaoDC = 0;
+float TensaoDC = 0;
+//valor resistor
+float voltsporUnidade = 0.004700000;
+float R1 = 30000.0;
+float R2 = 7500.0;
+
+void setup() {
+  
+  
+  
+  //iniciar ethernet shield
+  Ethernet.begin(mac, ip);
+  server.begin();
+  
+  //coisas de serial
+  //Serial.begin(9600);
+  //Serial.println("Servidor Servo");
+
+  //atribuição do servo
+  alfa.attach(12);
+  alfa.write(80);
+  
+}
+
+void loop() {
+  // Essa parte eu não alterei nada
+  EthernetClient client = server.available();
+  if (client) {
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+
+        
+        if (readString.length() < 100) {
+
+          
+          readString += c; 
+          //Serial.print(c);
+        } 
+
+        //if HTTP request has ended
+        if (c == '\n') {
+
+          ///////////////
+          //Serial.println(readString); 
+
+          client.println("HTTP/1.1 200 OK"); 
+          client.println("Content-Type: text/html");
+          client.println();
+
+          client.println("<HTML>");
+          client.println("<HEAD>");
+          //client.println("<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>");
+
+          client.println("<TITLE>Controle de energia Arduino</TITLE>");
+          client.println("</HEAD>");
+          client.println("<BODY>");
+          client.println("<H1>Controle de energia e monitoramento de voltagem</H1>");
+                     
+                      //conta do voltimetro  
+          mediaTotalTensaoDC = 0;
+            for (int i = 0; i < amostragem ; i++) {
+              valorTensaoDC = analogRead(sensorTensaoDC);
+              valorTensaoDC = (valorTensaoDC * voltsporUnidade);
+              mediaTotalTensaoDC = mediaTotalTensaoDC + (valorTensaoDC / (R2 / (R1 + R2)));
+              
+            }
+          
+          TensaoDC = mediaTotalTensaoDC / amostragem;
+            //Serial.print("Valor: ");
+            //Serial.print(valorFinalTensaoDC);
+            //Serial.print(" V");
+          
+          
+          //client.println("________________________________");
+
+          //script para atualizar a div (obrigado stackoverflow)
+          //client.println("<script>");
+          //client.println("$(document).ready(function(){");
+          //client.println("setInterval(function(){");
+          //client.println("$('#here').load(window.location.href + ' #here' );");
+          //client.println("}, 1000);");
+          //client.println("});");
+          //client.println("</script>");
+          //-------------------------------------
+          
+          client.println("<div id='here'>");
+          client.println("<br>");
+          //voltimentro
+          client.println("<br>Voltimentro: ");
+          client.println("<b>");
+          client.println(TensaoDC);
+          client.println("</b>");
+          client.println(" V");
+          
+          // status de leitura
+          if (TensaoDC < 10.00) {
+             client.println("<b><p style='color:#B91E1E';>Inoperante</p></b>");
+          }
+          else if (TensaoDC >= 10.01 && TensaoDC <= 11.99) {
+            client.println("<b><p style='color:#A2AC07';>Alerta</p></b>");
+          }
+          else {
+            client.println("<b><p style='color:#25B91E';>OK</p></b>");
+          }
+          client.println("</div>");
+          //client.println("<br>________________________________");
+          client.println("<br>");
+          client.println("<br>Controle de energia");
+          client.println("<br>");
+          client.println("<a href=\"/?on\"\"><button>Ligar</button></a>"); 
+          client.println("<a href=\"/?off\"\"><button>Desligar</button></a><br>"); 
+          client.println("<br> ");
+          client.println("");
+          
+          
+ 
+
+              // controle do arduino
+          if(readString.indexOf("?on") >0)//checks for on
+          {
+            client.println("Voce ligou a energia");
+            status = 1;
+            alfa.write(80); // mude para o angulo de preferencia
+            
+
+          }
+          if(readString.indexOf("?off") >0)//checks for off
+          {
+            client.println("Voce desligou a energia");
+            status = 0;
+            alfa.write(180); // mude para o angulo de preferencia
+          }
+
+          client.println("<br>A energia esta atualmente: ");
+          
+          if(status == 0){
+            client.println("<b>Desligada");
+          } else {
+            client.println("<b>Ligada");
+          }
+          
+          
+          client.println("</BODY>");
+          client.println("</HTML>");
+          
+          delay(100);
+          client.flush();
+          client.stop();
+          readString="";
+          //COMANDOS QUE EU TIREI DO CLIENT:
+          //Serial.println("Energia Ligada");
+          //Serial.println("Energia Desligada");
+        }
+      }
+    }
+  }
+} 
